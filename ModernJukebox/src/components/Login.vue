2519c5606c4fd2a19d9b7580107da57042d74f22
Dennis Schmidt <me@dennisschmidt.net>
2017-08-19 13:38:47 +0200

    Spotify login works...maybe

--- ModernJukebox/src/components/Login.vue
+++ ModernJukebox/src/components/Login.vue
@@ -6,9 +6,44 @@
 
 <script>
   export default {
+    data() {
+      return {
+        client_id: 'acda5dc270674c59be88eca853c1d4ff', // Your client id
+        redirect_uri: 'http://localhost:8080/', // Your redirect uri
+        state: this.generateRandomString(16),
+        scope: 'user-read-private user-read-email',
+        url: 'https://accounts.spotify.com/authorize',
+        stateKey: 'spotify_auth_state',
+      }
+    },
     methods: {
       login() {
+        localStorage.setItem(this.stateKey, this.state),
+        this.url += '?response_type=token',
+        this.url += '&client_id=' + encodeURIComponent(this.client_id),
+        this.url += '&scope=' + encodeURIComponent(this.scope),
+        this.url += '&redirect_uri=' + encodeURIComponent(this.redirect_uri),
+        this.url += '&state=' + encodeURIComponent(this.state),
+        window.location = this.url,
         this.$emit('loggedIn')
+      },
+      generateRandomString(length) {
+        var text = '';
+        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (var i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+      },
+      getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while ( e = r.exec(q)) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
       }
     }
   }
