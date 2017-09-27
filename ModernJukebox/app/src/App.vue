3d7a0282f874674a4b57bbe0144d94a9dceb3625
Dennis Schmidt <me@dennisschmidt.net>
2017-09-27 15:35:09 +0200

    Trying auth on my own

--- ModernJukebox/app/src/App.vue
+++ ModernJukebox/app/src/App.vue
@@ -1,35 +1,73 @@
 <template>
   <div id="app">
     <search v-if="loggedIn"></search>
-    <login @loggedIn="login"></login>
+    <button @click="login">Login</button>
   </div>
 </template>
 
 <script>
-
-
   // import Hello from './components/Hello.vue';
   import Search from './components/Search.vue'
-  import Login from './components/Login.vue'
+  // import Login from './components/Login.vue'
+
+  var SpotifyWebApi = require('spotify-web-api-node');
+
+  var scopes = ['user-read-private', 'user-read-email'],
+  state = 'some-state-of-my-choice';
+
+  // credentials are optional
+  var spotifyApi = new SpotifyWebApi({
+    clientId : 'acda5dc270674c59be88eca853c1d4ff',
+    clientSecret : '804ae867f1ad414ab4b2f1a3b68f4bcc',
+    redirectUri : 'http://localhost:8080/'
+  });
+
+  // Create the authorization URL
+  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+  // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+  // console.log(authorizeURL);
 
   export default {
     name: 'app',
     data() {
       return {
-        loggedIn: false
+        loggedIn: false,
+        access_token: '',
+        data: ''
       }
     },
     components: {
-      'search': Search,
-      'login': Login
+      'search': Search
+      // 'login': Login
     },
     methods: {
       login() {
-        // this.loggedIn = true
+        window.location = authorizeURL,
+
+        this.$http.post('https://accounts.spotify.com/api/token', {
+          form: {
+            grant_type: 'authorization_code',
+            code: window.location.search.replace("?code=", "").split("&")[0],
+            redirect_uri: redirectUri
+          },
+          headers: {
+            Authorization: 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+          }
+        }),
+
+        // this.access_token = 'BQDuPEerrxdttIcSHbZCYlre3tngjI_9UX1Nt4AezRIbgy-vJndUhjTDeQZJjcS_8YntE7OLoqhK3GI1aZaXg-eoCze8s9M3rMyzmEefgE-ZIHEJrFkAhOgoO69sllTkS82TwIIWdwuEpqE',
+        // spotifyApi.setAccessToken(this.access_token),
+        // console.log(this.access_token),
+        spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+          .then(function(data) {
+            console.log('Artist albums', data.body);
+          }, function(err) {
+            console.error(err);
+          });
       }
     },
     created() {
-      this.$http.get('https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg')
       // console.log('https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg');
     }
   }
--- ModernJukebox/app/src/components/Login.vue
+++ ModernJukebox/app/src/components/Login.vue
@@ -5,27 +5,6 @@
 </template>
 
 <script>
-  var SpotifyWebApi = require('spotify-web-api-node');
-
-  var scopes = ['user-read-private', 'user-read-email'],
-    redirectUri = 'http://localhost:8080/',
-    clientId = 'acda5dc270674c59be88eca853c1d4ff',
-    state = 'some-state-of-my-choice';
-
-  // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-  var spotifyApi = new SpotifyWebApi({
-    redirectUri: redirectUri,
-    clientId: clientId
-  });
-
-  // Create the authorization URL
-  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
-
-  // window.location = authorizeURL;
-
-  // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-  console.log(authorizeURL);
-
   export default {
     data() {
       return {
@@ -33,7 +12,7 @@
     },
     methods: {
       login() {
-        window.location = authorizeURL;
+        // window.location = authorizeURL;
       }
     }
   }
