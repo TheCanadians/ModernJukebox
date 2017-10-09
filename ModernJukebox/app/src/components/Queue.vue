7e3e9bae9acb312f4d0b14483824fda1339b0609
Unknown <me@dennisschmidt.net>
2017-10-09 17:31:03 +0200

    Changed Firebase console.log to something useful

---
+++ ModernJukebox/app/src/components/Queue.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="queue">
+    <ul>
+      <li>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  // Initialize Firebase
+  /* var config = {
+    apiKey: "AIzaSyCW-yHDVzJT7IgK6exI1AElYZ85BKiKeBc",
+    authDomain: "modern-jukebox.firebaseapp.com",
+    databaseURL: "https://modern-jukebox.firebaseio.com",
+    projectId: "modern-jukebox",
+    storageBucket: "",
+    messagingSenderId: "560034994179"
+  };
+  firebase.initializeApp(config); */
+
+  // Create references
+  // const dbRefObject = firebase.database().ref().child('schweinske-dehnhaide').child('limit');
+
+  // Sync object changes
+  // dbRefObject.on('value', snap => console.log(snap.val()));
+
+  export default {
+    data() {
+      return {
+        data: ''
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
